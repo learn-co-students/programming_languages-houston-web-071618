de956de3a2be9914e5765e37a9d2a5fc2196761d
Marylene Chan <marylenechan@marylenes-mbp-2.attlocal.net>
2018-07-07 17:22:16 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,68 @@
+languages_hash = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+
+  languages.each do | key, data | 
+    if key == :oo
+    data.each do | lang_name, lang_type |
+      new_hash[lang_name]
+      new_hash[lang_name] = lang_type
+      new_hash[lang_name][:style] = [:oo]
+    end
+    end 
+    if key == :functional
+    data.each do | lang_name, lang_type |
+      new_hash[lang_name]
+      new_hash[lang_name] = lang_type
+      new_hash[lang_name][:style] = [:functional]
+    end
+    end   
+    data.each do | lang_name, lang_type |
+      if lang_name == :javascript
+      new_hash[lang_name]
+      new_hash[lang_name] = lang_type
+      new_hash[lang_name][:style] = [:oo, :functional]
+    end 
+    end
+  end
+# return my new_hash
+new_hash
 end
+reformat_languages(languages_hash)
+
+
+
+
+
