6b0dbe13cb56ce849a385e5f9082153725be48c7
Richard Ojo <richardojo04@gmail.com>
2018-07-15 19:34:52 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  if languages[:oo]
+    languages[:oo].values.each do |v|
+      v.merge!(:style => [:oo])
+    end
+    languages[:oo]
+  else
+    languages[:functional].values.each do |v|
+      v.merge!(:style => [:functional])
+    end
+    languages[:functional]
+  end
 end
